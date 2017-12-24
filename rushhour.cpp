19983b8e9b336563a511ece0a4ef7ad49882d059
Jemspeth <spether13@gmail.com>
2017-12-24 12:42:15 -0800

    Update rushhour.cpp

--- rushhour.cpp
+++ rushhour.cpp
@@ -1,23 +1,22 @@
-/** @file rushhour.cpp
-@author Arush Prashar
-@version Revision 1.1
-@brief This program solves the Rush Hour Game. 
-@date Tuesday, October 03, 2017
+/** @file RushHour.cpp
+@author Arush Prashar & Jeremy Speth
+@version Revision 1.4
+@brief This program solves the Rush Hour Game using a Breadth-First-Search. 
+@date Tuesday, December 5, 2017
 */
 
 /**
 @mainpage
-This program gets user input for the vehicles that are to be placed on the board
-and after that using the board.cpp file the program sets up the board and solves 
-it and it prints out the least number of moves required to solve the board,
-there is a 10 vehicles and 10 moves cap on the program if the board can not be
-solved or there is no solution under 11 moves the program outputs you didn't win.  
+This program gets user input for the vehicles that are to be placed on a 6x6
+board. The game is then solved using a Breadth-First-Search and the number of
+moves that it took to beat the game is output to the terminal. Multiple scenarios
+can be input by the user. There is a cap of 18 vehicles to be placed on the board.  
 */
 
 //
 // Header Files ///////////////////////////////////////////////////
 //
-#include "arushBoard.h"
+#include "Board.h"
 
 
 //
@@ -30,15 +29,16 @@ int main()
 	    sz = 0, //this is the size of the vehicle that can be 2 or 3.
 	    rw = 0, //this tells in which row the vehicle is placed or the row with its topmost end
 	    col = 0; //this tells in which column the vehicle is placed or the column with its leftmost end
-	    board brd;  //this is the board which is created after getting user input. 
+	board brd;  //this is the board which is created after getting user input. 
 	char ornt;  //this tells whether the vehicle is horizontal 'H' or vertical 'V'
+
 	//variables passed into to the functions for output handling operations.
 	bool win = false; //this variable is marked true if their is a solution under 11 moves.
-	int cap = 11,	//this is the maximum moves that the program can use to solve the board.
-	    moves = 0;  //this variable is used to keep the number of moves that the program has used to solve the problem.
+	int moves = 0;  //this variable is used to keep the number of moves that the program has used to solve the problem.
 	int scenario=1; //this keeps track of the scenario number being solved currently
 	string str;
 	cin >> numVehicles;
+
 	while( numVehicles != 0 )
 	{
 		brd.setCars( numVehicles );
@@ -49,20 +49,22 @@ int main()
 			brd.setVehicle( sz, toupper( ornt ), rw, col, i );
 		}
 		brd.setBoard();
-		brd.b2string( str );
-		cout << str << endl;
-		brd.solve( moves, cap, win );
+
+		brd.solve( moves, win );
+
 		if( win == true )
 		{
-			cout << "Scenario " << scenario << " requires " << cap << " moves" << endl;
+			cout << "Scenario " << scenario << " requires " << moves << " moves" << endl;
 		}
 		else
 		{
 			cout << "You did not win" << endl;
 		}
+
 		// resetting the variables passed in to function with reference for use for next scenario. 
 		win = false;
-		cap = 11;
+		brd.resetBoard();
+		moves = 0;
 		++scenario;
 		cin >> numVehicles;
 	}
